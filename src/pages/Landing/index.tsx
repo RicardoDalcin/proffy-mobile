9762c6438ef162ebb5dca07e81c49990cd93ea97
Ricardo <ricardo.dalcinh@gmail.com>
2020-08-06 11:44:04 -0300

    Created teacher list/favorites header + navigation

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
     navigate('GiveClasses')
   }
 
+  const handleNavigateToStudyPages = () => {
+    navigate('Study')
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -27,7 +31,10 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton
+          style={[styles.button, styles.buttonPrimary]}
+          onPress={handleNavigateToStudyPages}
+        >
           <Image source={studyIcon} />
 
           <Text style={styles.buttonText}>Estudar</Text>
